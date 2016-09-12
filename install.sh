bc881f829d4b823fd7f3662844824ee9f16e53f5
Brady Sullivan <brady@bsull.com>
2016-09-12 09:15:09 +0000

    Fill out GOPATH with appropriate directories in install.

--- install.sh
+++ install.sh
@@ -2,8 +2,15 @@
 
 cp vimrc ~/.vimrc
 cp tmux.conf ~/.tmux.conf
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp zshrc ~/.zshrc
 cp d1str0.zsh-theme ~/.oh-my-zsh/themes/d1str0.zsh-theme
+
+# Fill out GoPath directories
+mkdir ~/Golang/src
+mkdir ~/Golang/pkg
+mkdir ~/Golang/bin
+
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
