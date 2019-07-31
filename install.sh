4fe2295638018184ab11d272ca6766bbac95ce6b
Brady Sullivan <brady@bsull.com>
2019-07-31 10:59:28 -0700

    fix: stop oh-my-zsh from immediately launching zsh

--- install.sh
+++ install.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Fill out GoPath directories
-mkdir ~/go
-mkdir ~/go/src
-mkdir ~/go/pkg
-mkdir ~/go/bin
-
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
@@ -16,6 +10,8 @@ echo "|0|0|0|" >> ~/.motd
 
 cp vimrc ~/.vimrc
 cp tmux.conf ~/.tmux.conf
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
+chsh zsh
 cp zshrc ~/.zshrc
 cp d1str0.zsh-theme ~/.oh-my-zsh/themes/d1str0.zsh-theme
+zsh
