aa231b933f1f383ffa8c526d2b571a846d975f43
Brady Sullivan <brady@bsull.com>
2019-08-28 16:30:31 -0700

    Fix install for chsh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+rm ~/.motd
 touch ~/.motd
 echo "|_|0|_|" >> ~/.motd
 echo "|_|_|0|" >> ~/.motd
@@ -11,7 +12,7 @@ echo "|0|0|0|" >> ~/.motd
 cp vimrc ~/.vimrc
 cp tmux.conf ~/.tmux.conf
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
-chsh zsh
+chsh -s zsh $(whoami)
 cp zshrc ~/.zshrc
 cp d1str0.zsh-theme ~/.oh-my-zsh/themes/d1str0.zsh-theme
 zsh
